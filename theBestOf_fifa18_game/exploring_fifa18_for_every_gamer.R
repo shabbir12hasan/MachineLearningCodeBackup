690f2c60ca121ecd105037f93f4bff66be861348
Hasan <shabbir12hasan@gmail.com>
2018-05-01 00:31:55 +1000

    formating

--- theBestOf_fifa18_game/exploring_fifa18_for_every_gamer.R
+++ theBestOf_fifa18_game/exploring_fifa18_for_every_gamer.R
@@ -1,10 +1,20 @@
-#importing libraries
+##########################
+### importing Libraries ###
+##########################
+library(tidyr)
 
-
-#importing data
+##########################
+### importing data ###
+##########################
 df <- read.csv("theBestOf_fifa18/CompleteDataset.csv", encoding = "UTF-8")
-View(head(df))
 
+
+##########################
+### Cleaning and future engineering ###
+##########################
 #Taking only required data
 df <-df[,c(2,3,5,7,8,9,11,12,15,42,43,45,64)]
 View(head(df))
+
+#prefered position as first choice
+df <- separate(df, Preferred.Positions, into = c("Prefered.Position"), extra = "drop",sep = " ")
