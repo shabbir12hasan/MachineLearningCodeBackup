383f79eaf014e19aa16a2e5f3efe6dd972cdb76d
Shabbir Hasan <shabbir.hasan@iselect.com.au>
2018-12-05 17:26:08 +1100

    update

--- Reinforcement Learning/frozenLake_test3.py
+++ Reinforcement Learning/frozenLake_test3.py
@@ -4,9 +4,18 @@ import sys
 import time
 from IPython.display import clear_output
 
+from gym.envs.registration import register
+register(
+    id='FrozenLakeNotSlippery-v0',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery': False},
+    max_episode_steps=100,
+    reward_threshold=0.78, # optimum = .8196
+)
 
-env = gym.make('FrozenLake-v0')
-env.seed(0)
+
+env = gym.make('FrozenLakeNotSlippery-v0')
+env.seed(30)
 np.random.seed(1234)
 
 #Initialize table with all zeros, this is our mapping from states to actions,
@@ -27,56 +36,19 @@ for i in range(num_episodes):
     state = env.reset()  # Reset environment and get first new observation
 
     for iter in range(num_iter):
-        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+        print(env.render())
+        action = np.argmax(Q[state, :] + np.random.randn(1,4 ))
+        print(env.step(action))
+        # print(env.render())
+
+
         observed_state, reward, done, info = env.step(action)
         Q[state, action] = (1 - learning_rate) * Q[state, action] + learning_rate * (reward + discount* np.max(Q[observed_state, :]))
         state = observed_state
 
+        if done: break
 
-
-
-    rAll = 0  # initial value for rewards at all SxA situations, closer to one encourages exploring
-    d = False
-    step_ = 0
-    rewards = []
-    # The Q-Table learning algorithm
-    while step_ < 99:
-        step_ += 1
-
-        # Choose an action by greedily (with noise) picking from Q table,
-        # this distribution will converge to the true expected value in the long term as num episodes -> infinity :)
-        env.render()
-
-
-
-#reward is being 0 always
-
-
-        # Using random noise
-        # action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (i + 1))
-
-        clear_output(wait=2)
-
-
-        time.sleep(0.1)
-        # Get new state and reward from environment
-        observed_state, reward, done, info = env.step(action)
-        rewards.append(reward)
-        print('action = {}, location = {}, episode number: {}'.format(action_map[action], observed_state, i))
-        # Update Q-Table with new knowledge
-        Q[state, action] = Q[state, action] + lr * (reward + y * np.max(Q[observed_state, :]) - Q[state, action])
-        # Q[state, action] = (1-lr)*Q[state, action] + lr*(reward+y*np.max(Q[observed_state, :]))
-        rAll += reward
-
-        state = observed_state
-
-        if done == True:
-            break
-
-    # jList.append(j)
-    rList.append(rAll)
-
-print("Score over time: " + str(sum(rList) / num_episodes))
-print("Final Q-Table Values:\n\n", Q)
+    print(np.argmax(Q, axis=1))
+    print(np.around(Q, 6))
 
 
