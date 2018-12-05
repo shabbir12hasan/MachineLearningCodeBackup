cbe9cc43153fc41020ca5164646fb9ed28cff489
Shabbir Hasan <shabbir.hasan@iselect.com.au>
2018-12-05 16:46:23 +1100

    frozenlake test RL

--- Reinforcement Learning/frozenLake_test3.py
+++ Reinforcement Learning/frozenLake_test3.py
@@ -4,34 +4,37 @@ import sys
 import time
 from IPython.display import clear_output
 
-#import seaborn as sns
-# sns.set_style('whitegrid')
-# sns.set_palette('viridis')
-
-
 
 env = gym.make('FrozenLake-v0')
+env.seed(0)
+np.random.seed(1234)
 
 #Initialize table with all zeros, this is our mapping from states to actions,
 #  agent will learn rewards for each pair as it explores the world
 #  each col is an action, each row is a state of the world. the values in this table are expected reward.
-Q = np.zeros([env.observation_space.n,env.action_space.n])
+Q = np.zeros([env.observation_space.n, env.action_space.n])
 
 # Set learning parameters
-lr = .8  # this is how quickly we overwrite existing values with recent learning/experiences
-y = .95 # this is the short vs long-term bias.
+learning_rate = .8  # this is how quickly we overwrite existing values with recent learning/experiences
+discount = .95 # this is the short vs long-term bias.
 # if y (gamma) is small, then view is very short-term. if large, agent will have long-term .
-num_episodes = 2000
+num_episodes = 5000
+num_iter = 2000
 
-#create lists to contain total rewards and steps per episode
-#jList = []
-rList = []
-
-action_map = ['up', 'right', 'down', 'left']
 
 for i in range(num_episodes):
 
     state = env.reset()  # Reset environment and get first new observation
+
+    for iter in range(num_iter):
+        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+        observed_state, reward, done, info = env.step(action)
+        Q[state, action] = (1 - learning_rate) * Q[state, action] + learning_rate * (reward + discount* np.max(Q[observed_state, :]))
+        state = observed_state
+
+
+
+
     rAll = 0  # initial value for rewards at all SxA situations, closer to one encourages exploring
     d = False
     step_ = 0
@@ -43,7 +46,7 @@ for i in range(num_episodes):
         # Choose an action by greedily (with noise) picking from Q table,
         # this distribution will converge to the true expected value in the long term as num episodes -> infinity :)
         env.render()
-        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+
 
 
 #reward is being 0 always
