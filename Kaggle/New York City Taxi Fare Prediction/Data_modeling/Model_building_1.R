63763dc903a31854b2e6723c8dd0e26c8abc12a9
Shabbir Hasan <shabbir12hasan@gmail.com>
2019-02-01 15:21:01 +1100

    bigupdate

--- Kaggle/New York City Taxi Fare Prediction/Data_modeling/Model_building_1.R
+++ Kaggle/New York City Taxi Fare Prediction/Data_modeling/Model_building_1.R
@@ -1,13 +1,22 @@
-data <- read.csv('train_2.csv')
+data <- read.csv('/Users/shabbir.hasan/Documents/Hasan/Git Repositories/shabbir12hasan/MachineLearningCodeBackup/Kaggle/New York City Taxi Fare Prediction/Data_cleaning/clean_v1_31-01.csv')
 
 head(data)
 
 ########################################################## FEATURE ENGINEERING ######################################################################
 
+summary(data)
 
 
+#check data types
+sapply(data,class) 
+
+
+
+data$time_15_min_bin <- as.factor(data$time_15_min_bin)
+data$time_30_min_bin <- as.factor(data$time_30_min_bin)
+data$hour <- as.factor(data$hour)
 data$weekend_weekday <- as.factor(data$weekend_weekday)
-data$date <- as.Date(data$date)
+data$day_of_week <- as.factor(data$day_of_week)
 data$day_of_month <- as.factor(data$day_of_month)
 data$month <- as.factor(data$month)
 data$year <- as.factor(data$year)
@@ -15,8 +24,9 @@ data$passenger_count <- as.factor(data$passenger_count)
 
 
 
+summary(data)
 
-
+#check data types
 str(data)
 
 
@@ -48,8 +58,19 @@ head(test)
 
 ########################################################## MODEL CREATION #######################################################################
 
+#linear reg
+model_linearreg = lm(train$fare_amount ~ ., data = train)
+
 
 #RANDOM FOREST
 #install.packages("randomForest")
 library(randomForest)
 mode_rf = randomForest(train$fare_amount ~ . , data = train)
+
+summary(mode_rf)
+
+
+
+
+
+
